d5241c1d338e10d4f3e5b90ded9944818c4c75a9
careym04 <mark.carey@bbc.co.uk>
2024-04-01 00:33:53 +0100

    chapter 9 complete all tests passed

--- venv/tango_with_django_project/rango/models.py
+++ venv/tango_with_django_project/rango/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     
@@ -33,4 +35,14 @@ class Page(models.Model):
         return self.title
 
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile-images', blank=True)
+    
+    
+    def __str__(self):
+        return self.user.username
+
--- venv/tango_with_django_project/tango_with_django_project/settings.py
+++ venv/tango_with_django_project/tango_with_django_project/settings.py
@@ -24,6 +24,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
+LOGIN_URL = 'rango:login'
+
 
 
 # Quick-start development settings - unsuitable for production
