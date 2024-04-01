88b5f53de88ca0adaa7cf50c00ffb8378d17143b
careym04 <mark.carey@bbc.co.uk>
2024-04-01 01:28:47 +0100

    chapter 10 complete all tests passed

--- venv/tango_with_django_project/rango/views.py
+++ venv/tango_with_django_project/rango/views.py
@@ -14,11 +14,13 @@ from django.shortcuts import redirect
 
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 
 
 
 def index(request):
-    
+    request.session.set_test_cookie()
     
     
     category_list = Category.objects.order_by('-likes')[:5]
@@ -30,14 +32,28 @@ def index(request):
     context_dict['pages'] = page_list
     
     
+    visitor_cookie_handler(request)
     
-    return render(request, 'rango/index.html', context=context_dict)
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+    
+    
+   
 
+
+
+        
+        
+        
 
 
 def about(request):
     
+    
     context_dict = {'boldmessage': 'mark carey'}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     
     return render(request, 'rango/about.html', context=context_dict)
 
@@ -182,3 +198,24 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
